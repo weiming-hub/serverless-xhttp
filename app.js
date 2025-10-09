60b9bf388defb45c1a1834c9b3bde0eadf4f5db6
weiming <88963227+weiming-hub@users.noreply.github.com>
2025-10-09 23:58:59 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const { Buffer } = require('buffer');
 const { exec, execSync } = require('child_process');
 
 // 环境变量
-const UUID = process.env.UUID || 'a2056d0d-c98e-4aeb-9aab-37f64edd5710'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
-const NEZHA_SERVER = process.env.NEZHA_SERVER || '';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
+const UUID = process.env.UUID || '03e46db9-8bd6-4dcc-abac-6203af25a4c1'; // 使用哪吒v1，在不同的平台部署需修改UUID，否则会覆盖
+const NEZHA_SERVER = process.env.NEZHA_SERVER || 'jj.jaiyu.dpdns.org:8008';       // 哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';           // 哪吒v1没有此变量，v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls
-const NEZHA_KEY = process.env.NEZHA_KEY || '';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
-const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
+const NEZHA_KEY = process.env.NEZHA_KEY || '7Ner02g5Khs71ghjlSZGfjKjdFilje5K';             // v1的NZ_CLIENT_SECRET或v0的agent端口  
+const AUTO_ACCESS = process.env.AUTO_ACCESS || ture;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
 const XPATH = process.env.XPATH || UUID.slice(0, 8);       // xhttp路径,自动获取uuid前8位
 const SUB_PATH = process.env.SUB_PATH || `${UUID}`;        // 节点订阅路径,默认位uuid
-const DOMAIN = process.env.DOMAIN || '';                   // 域名或ip,留空将自动获取服务器ip
+const DOMAIN = process.env.DOMAIN || 'https://qwe8899-x.hf.space';                   // 域名或ip,留空将自动获取服务器ip
 const NAME = process.env.NAME || 'Hug';                    // 节点名称
 const PORT = process.env.PORT || 7860;                     // http服务                   
 
